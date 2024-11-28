ffe2c11148d6dc4861516a609c0a8792106bf095
MarianVentura <marianv1119@gmail.com>
2024-11-28 16:22:02 -0400

    Modificacion en el MantenimientoVehiculoService

--- Services/MantenimientoVehiculoService.cs
+++ Services/MantenimientoVehiculoService.cs
@@ -2,56 +2,62 @@
 using CarRental.Data;
 using CarRental.Models;
 
-namespace CarRental.Services;
-
-public class MantenimientoVehiculoService
+namespace CarRental.Services
 {
-    private readonly Contexto _context;
-
-    public MantenimientoVehiculoService(Contexto context)
+    public class MantenimientoVehiculoService
     {
-        _context = context;
-    }
+        private readonly IDbContextFactory<Contexto> _dbContextFactory;
 
-    public async Task<List<MantenimientoVehiculo>> ObtenerMantenimientosAsync()
-    {
-        return await _context.MantenimientoVehiculo
-            .Include(m => m.Vehiculo)
-            .AsNoTracking()
-            .ToListAsync();
-    }
-
-    public async Task<MantenimientoVehiculo?> ObtenerMantenimientoPorIdAsync(int id)
-    {
-        return await _context.MantenimientoVehiculo
-            .Include(m => m.Vehiculo)
-            .AsNoTracking()
-            .FirstOrDefaultAsync(m => m.MantenimientoId == id);
-    }
-
-    public async Task<bool> CrearMantenimientoAsync(MantenimientoVehiculo mantenimiento)
-    {
-        _context.MantenimientoVehiculo.Add(mantenimiento);
-        await _context.SaveChangesAsync();
-        return true;
-    }
-
-    public async Task<bool> ActualizarMantenimientoAsync(MantenimientoVehiculo mantenimiento)
-    {
-        _context.MantenimientoVehiculo.Update(mantenimiento);
-        await _context.SaveChangesAsync();
-        return true;
-    }
-
-    public async Task<bool> EliminarMantenimientoAsync(int id)
-    {
-        var mantenimiento = await _context.MantenimientoVehiculo.FindAsync(id);
-        if (mantenimiento != null)
+        public MantenimientoVehiculoService(IDbContextFactory<Contexto> dbContextFactory)
         {
-            _context.MantenimientoVehiculo.Remove(mantenimiento);
-            await _context.SaveChangesAsync();
+            _dbContextFactory = dbContextFactory;
+        }
+
+        public async Task<List<MantenimientoVehiculo>> ObtenerMantenimientosAsync()
+        {
+            await using var contexto = await _dbContextFactory.CreateDbContextAsync();
+            return await contexto.MantenimientoVehiculo
+                .Include(m => m.Vehiculo)
+                .AsNoTracking() // Mejor rendimiento sin seguimiento de cambios
+                .ToListAsync();
+        }
+
+        public async Task<MantenimientoVehiculo?> ObtenerMantenimientoPorIdAsync(int id)
+        {
+            await using var contexto = await _dbContextFactory.CreateDbContextAsync();
+            return await contexto.MantenimientoVehiculo
+                .Include(m => m.Vehiculo)
+                .AsNoTracking()
+                .FirstOrDefaultAsync(m => m.MantenimientoId == id);
+        }
+
+        public async Task<bool> CrearMantenimientoAsync(MantenimientoVehiculo mantenimiento)
+        {
+            await using var contexto = await _dbContextFactory.CreateDbContextAsync();
+            contexto.MantenimientoVehiculo.Add(mantenimiento);
+            await contexto.SaveChangesAsync();
             return true;
         }
-        return false;
+
+        public async Task<bool> ActualizarMantenimientoAsync(MantenimientoVehiculo mantenimiento)
+        {
+            await using var contexto = await _dbContextFactory.CreateDbContextAsync();
+            contexto.MantenimientoVehiculo.Update(mantenimiento);
+            await contexto.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<bool> EliminarMantenimientoAsync(int id)
+        {
+            await using var contexto = await _dbContextFactory.CreateDbContextAsync();
+            var mantenimiento = await contexto.MantenimientoVehiculo.FindAsync(id);
+            if (mantenimiento != null)
+            {
+                contexto.MantenimientoVehiculo.Remove(mantenimiento);
+                await contexto.SaveChangesAsync();
+                return true;
+            }
+            return false;
+        }
     }
 }
