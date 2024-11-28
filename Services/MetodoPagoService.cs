cd3ff958b484e35163a67e8534569cd888e4e746
MarianVentura <marianv1119@gmail.com>
2024-11-28 16:22:15 -0400

    Modificacion en MetodoPagoService

--- Services/MetodoPagoService.cs
+++ Services/MetodoPagoService.cs
@@ -2,56 +2,62 @@
 using CarRental.Data;
 using CarRental.Models;
 
-namespace CarRental.Services;
-
-public class MetodoPagoService
+namespace CarRental.Services
 {
-    private readonly Contexto _context;
-
-    public MetodoPagoService(Contexto context)
+    public class MetodoPagoService
     {
-        _context = context;
-    }
+        private readonly IDbContextFactory<Contexto> _dbContextFactory;
 
-    public async Task<List<MetodoPago>> ObtenerMetodosPagoAsync()
-    {
-        return await _context.MetodoPago
-            .Include(m => m.Reserva)
-            .AsNoTracking()
-            .ToListAsync();
-    }
-
-    public async Task<MetodoPago?> ObtenerMetodoPagoPorIdAsync(int id)
-    {
-        return await _context.MetodoPago
-            .Include(m => m.Reserva)
-            .AsNoTracking()
-            .FirstOrDefaultAsync(m => m.MetodoPagoId == id);
-    }
-
-    public async Task<bool> CrearMetodoPagoAsync(MetodoPago metodoPago)
-    {
-        _context.MetodoPago.Add(metodoPago);
-        await _context.SaveChangesAsync();
-        return true;
-    }
-
-    public async Task<bool> ActualizarMetodoPagoAsync(MetodoPago metodoPago)
-    {
-        _context.MetodoPago.Update(metodoPago);
-        await _context.SaveChangesAsync();
-        return true;
-    }
-
-    public async Task<bool> EliminarMetodoPagoAsync(int id)
-    {
-        var metodoPago = await _context.MetodoPago.FindAsync(id);
-        if (metodoPago != null)
+        public MetodoPagoService(IDbContextFactory<Contexto> dbContextFactory)
         {
-            _context.MetodoPago.Remove(metodoPago);
-            await _context.SaveChangesAsync();
+            _dbContextFactory = dbContextFactory;
+        }
+
+        public async Task<List<MetodoPago>> ObtenerMetodosPagoAsync()
+        {
+            await using var contexto = await _dbContextFactory.CreateDbContextAsync();
+            return await contexto.MetodoPago
+                .Include(m => m.Reserva)
+                .AsNoTracking() // Mejor rendimiento sin seguimiento de cambios
+                .ToListAsync();
+        }
+
+        public async Task<MetodoPago?> ObtenerMetodoPagoPorIdAsync(int id)
+        {
+            await using var contexto = await _dbContextFactory.CreateDbContextAsync();
+            return await contexto.MetodoPago
+                .Include(m => m.Reserva)
+                .AsNoTracking()
+                .FirstOrDefaultAsync(m => m.MetodoPagoId == id);
+        }
+
+        public async Task<bool> CrearMetodoPagoAsync(MetodoPago metodoPago)
+        {
+            await using var contexto = await _dbContextFactory.CreateDbContextAsync();
+            contexto.MetodoPago.Add(metodoPago);
+            await contexto.SaveChangesAsync();
             return true;
         }
-        return false;
+
+        public async Task<bool> ActualizarMetodoPagoAsync(MetodoPago metodoPago)
+        {
+            await using var contexto = await _dbContextFactory.CreateDbContextAsync();
+            contexto.MetodoPago.Update(metodoPago);
+            await contexto.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<bool> EliminarMetodoPagoAsync(int id)
+        {
+            await using var contexto = await _dbContextFactory.CreateDbContextAsync();
+            var metodoPago = await contexto.MetodoPago.FindAsync(id);
+            if (metodoPago != null)
+            {
+                contexto.MetodoPago.Remove(metodoPago);
+                await contexto.SaveChangesAsync();
+                return true;
+            }
+            return false;
+        }
     }
 }
