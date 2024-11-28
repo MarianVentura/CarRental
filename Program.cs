d85b3c8e7ea0a8701bddef37e38754ef55dc011f
MarianVentura <marianv1119@gmail.com>
2024-11-28 11:52:15 -0400

    Inyeccion del contexto en el program.cs

--- Program.cs
+++ Program.cs
@@ -24,13 +24,13 @@ builder.Services.AddAuthentication(options =>
     .AddIdentityCookies();
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
+builder.Services.AddDbContextFactory<Contexto>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddRoles<IdentityRole>()
-    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddEntityFrameworkStores<Contexto>()
     .AddSignInManager()
     .AddDefaultTokenProviders();
 
