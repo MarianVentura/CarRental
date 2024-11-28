6f2baaf900bbbddd1bbdc363deeee1a12c56662d
MarianVentura <marianv1119@gmail.com>
2024-11-28 10:27:25 -0400

    Inyectar modelos en el Contexto

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,3 +1,4 @@
+using CarRental.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -5,5 +6,12 @@ namespace CarRental.Data
 {
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
     {
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        public DbSet<Clientes> Clientes { get; set; }
+        public DbSet<MetodoPago> MetodoPago { get; set; }
+        public DbSet<Vehiculos> Vehiculos { get; set; }
+        public DbSet<Categorias> Categorias { get; set; }
+        public DbSet<MantenimientoVehiculo> MantenimientoVehiculo { get; set; }
+        public DbSet<Reservas> Reservas { get; set; }
     }
 }
