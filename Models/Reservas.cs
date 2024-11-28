f0f0d77035b5b8a101f7655de133114a957a6115
MarianVentura <marianv1119@gmail.com>
2024-11-28 10:27:00 -0400

    Creacion modelo Reservas

---
+++ Models/Reservas.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace CarRental.Models
+{
+    public class Reservas
+    {
+        [Key]
+        public int ReservaId { get; set; }
+
+        [Required(ErrorMessage = "Este campo es obligatorio.")]
+        public int ClienteId { get; set; }
+
+        [ForeignKey("ClienteId")]
+        public Clientes? Cliente { get; set; }
+
+        [Required(ErrorMessage = "Este campo es obligatorio.")]
+        public int VehiculoId { get; set; }
+
+        [ForeignKey("VehiculosId")]
+        public Vehiculos? Vehiculo { get; set; }
+
+        [Required(ErrorMessage = "Este campo es obligatorio.")]
+        public DateTime FechaRecogida { get; set; }
+
+        [Required(ErrorMessage = "Este campo es obligatorio.")]
+        public DateTime FechaDevolucion { get; set; }
+
+        [Required(ErrorMessage = "Este campo es obligatorio.")]
+        public decimal TotalPrecio { get; set; }
+
+        [Required(ErrorMessage = "Este campo es obligatorio.")]
+        public string? Estado { get; set; } // Ejemplo: "Pendiente", "Confirmada", "Cancelada"
+
+        public string? LugarRecogida { get; set; }
+        public string? LugarEntrega { get; set; }
+        public string? Comentarios { get; set; }
+
+        public int? MetodoPagoId { get; set; }
+
+        [ForeignKey("MetodoPagoId")]
+        public MetodoPago? MetodoPago { get; set; }
+    }
+}
