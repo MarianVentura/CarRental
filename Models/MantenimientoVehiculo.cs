c6e3bb494da2a631bfb721c368510255c3f423b6
MarianVentura <marianv1119@gmail.com>
2024-11-28 10:26:36 -0400

    Creacion del modelo MantenimientoVehiculo

---
+++ Models/MantenimientoVehiculo.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace CarRental.Models;
+
+public class MantenimientoVehiculo
+{
+    [Key]
+    public int MantenimientoId { get; set; }
+
+    [Required(ErrorMessage = "Este campo es obligatorio.")]
+    public int VehiculoId { get; set; }
+
+    [ForeignKey("VehiculoId")]
+    public Vehiculos? Vehiculo { get; set; }
+
+    [Required(ErrorMessage = "Este campo es obligatorio.")]
+    public DateTime FechaMantenimiento { get; set; }
+
+    [Required(ErrorMessage = "Este campo es obligatorio.")]
+    public string Descripcion { get; set; } // Descripción del mantenimiento realizado
+
+    public decimal Costo { get; set; } // Costo del mantenimiento
+}
