221253e682ee7b510ee89bad6cf73629452b46cf
Andy Javier <andy_javier@ucne.edu.do>
2024-11-28 12:38:20 -0400

    creé el servicio de Categoria

---
+++ Services/CategoriasService.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CarRental.Data;
+using CarRental.Models;
+
+namespace CarRental.Services;
+
+public class CategoriasService
+{
+    private readonly Contexto _context;
+
+    public CategoriasService(Contexto context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<Categorias>> ObtenerCategoriasAsync()
+    {
+        return await _context.Categorias.AsNoTracking().ToListAsync();
+    }
+
+    public async Task<Categorias?> ObtenerCategoriaPorIdAsync(int id)
+    {
+        return await _context.Categorias.AsNoTracking().FirstOrDefaultAsync(c => c.CategoriaId == id);
+    }
+
+    public async Task<bool> CrearCategoriaAsync(Categorias categoria)
+    {
+        _context.Categorias.Add(categoria);
+        await _context.SaveChangesAsync();
+        return true;
+    }
+
+    public async Task<bool> ActualizarCategoriaAsync(Categorias categoria)
+    {
+        _context.Categorias.Update(categoria);
+        await _context.SaveChangesAsync();
+        return true;
+    }
+
+    public async Task<bool> EliminarCategoriaAsync(int id)
+    {
+        var categoria = await _context.Categorias.FindAsync(id);
+        if (categoria != null)
+        {
+            _context.Categorias.Remove(categoria);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+        return false;
+    }
+}
