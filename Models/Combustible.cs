45a460e0c4e225a24743863cb4339e4af99c3922
Andy Javier <andy_javier@ucne.edu.do>
2024-11-29 23:31:50 -0400

    añadí combustible

---
+++ Models/Combustible.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarRental.Models;
+
+public class Combustible
+{
+    [Key]
+    public int CombustibleId { get; set; }
+
+    [Required(ErrorMessage = "Este campo es obligatorio.")]
+    public string? Tipo { get; set; } // Ejemplo: "Gasolina", "Diesel", "Eléctrico"
+
+    [Required(ErrorMessage = "Este campo es obligatorio.")]
+    public string? Politica { get; set; } // Ejemplo: "Lleno/Lleno", "Lleno/Vacío"
+
+    [Required(ErrorMessage = "Este campo es obligatorio.")]
+    [Range(0, double.MaxValue, ErrorMessage = "El costo por litro debe ser un valor positivo.")]
+    public decimal CostoPorLitro { get; set; }
+
+    public string? Descripcion { get; set; } // Información adicional sobre la política o tipo de combustible
+}
