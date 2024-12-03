37157329a8790fe8ccb63bf2d8c9ecf92af2a493
MarianVentura <marianv1119@gmail.com>
2024-12-03 14:07:06 -0400

    Modificacion en los modelos

--- Models/MetodoPago.cs
+++ Models/MetodoPago.cs
@@ -3,31 +3,41 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CarRental.Models;
 
-
-
 public class MetodoPago
 {
     [Key]
     public int MetodoPagoId { get; set; }
 
-    [Required(ErrorMessage = "El tipo de pago es obligatorio.")]
-    [RegularExpression(@"^(Tarjeta de crédito|Transferencia Bancaria)$", ErrorMessage = "El tipo debe ser Tarjeta de crédito o Transferencia Bancaria.")]
-    public string Tipo { get; set; }
+    [Required(ErrorMessage = "El tipo de método de pago es obligatorio.")]
+    public TipoMetodoPago Tipo { get; set; }
 
-    [Required(ErrorMessage = "La fecha de transacción es obligatoria.")]
-    [DataType(DataType.DateTime)]
     public DateTime FechaTransaccion { get; set; }
 
-    [Required(ErrorMessage = "El monto es obligatorio.")]
-    [Range(0, double.MaxValue, ErrorMessage = "El monto debe ser un valor positivo.")]
+    [Range(1, double.MaxValue, ErrorMessage = "El monto debe ser mayor a 0.")]
     public decimal Monto { get; set; }
 
-    [Required(ErrorMessage = "La reserva es obligatoria.")]
+    [Required]
     [ForeignKey("Reserva")]
     public int ReservaId { get; set; }
+    public Reserva Reserva { get; set; }
 
     [Required(ErrorMessage = "El estado de la transacción es obligatorio.")]
-    [RegularExpression(@"^(Aprobada|Rechazada|Pendiente)$", ErrorMessage = "El estado debe ser Aprobada, Rechazada o Pendiente.")]
-    public string EstadoTransaccion { get; set; }
+    public TransaccionEstado EstadoTransaccion { get; set; }
 
+    public string ProveedorPago { get; set; }
+
+    // Enum para el tipo de método de pago
+    public enum TipoMetodoPago
+    {
+        TarjetaDeCredito,
+        TransferenciaBancaria
+    }
+
+    // Enum para el estado de la transacción
+    public enum TransaccionEstado
+    {
+        Aprobada,
+        Rechazada,
+        Pendiente
+    }
 }
