314f48a9adbb470394c24321aaf9699de14f7313
Andy Vladimir Javier Familia <andy_javier@ucne.edu.do>
2024-12-04 19:44:57 -0400

    tipo de Vehiculos

---
+++ Models/TipoVehiculos.cs
@@ -0,0 +1,17 @@
+﻿namespace CarRental.Models
+{
+    // Enum para el tipo de combustible
+    public enum TipoCombustible
+    {
+        Gasolina,
+        Diesel,
+        Gas
+    }
+
+    // Enum para el tipo de transmisión
+    public enum TipoTransmision
+    {
+        Mecanica,
+        Automatica
+    }
+}
