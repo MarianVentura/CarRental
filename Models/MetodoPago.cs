b1aea19eea5388209ff6ea5c013040df8463533f
MarianVentura <marianv1119@gmail.com>
2024-12-08 11:46:52 -0400

    Modifique el modelo metodo pago

--- Models/MetodoPago.cs
+++ Models/MetodoPago.cs
@@ -10,10 +10,9 @@ public class MetodoPago
 
     [Required(ErrorMessage = "El tipo de pago es obligatorio.")]
     [RegularExpression(@"^(Tarjeta de crédito|Transferencia Bancaria|Pago en Línea)$", ErrorMessage = "El tipo debe ser Tarjeta de crédito, Transferencia Bancaria o Pago en Línea.")]
-    public string Tipo { get; set; }
+    public string? Tipo { get; set; }
 
     [Required(ErrorMessage = "La fecha de transacción es obligatoria.")]
-    [DataType(DataType.DateTime)]
     public DateTime FechaTransaccion { get; set; }
 
     [Required(ErrorMessage = "El monto es obligatorio.")]
@@ -23,13 +22,13 @@ public class MetodoPago
     [Required(ErrorMessage = "La reserva es obligatoria.")]
     [ForeignKey("Reserva")]
     public int ReservaId { get; set; }
-    public virtual Reserva Reserva { get; set; }
+    public virtual Reserva? Reserva { get; set; }
 
     [Required(ErrorMessage = "El estado de la transacción es obligatorio.")]
     [RegularExpression(@"^(Aprobada|Rechazada|Pendiente)$", ErrorMessage = "El estado debe ser Aprobada, Rechazada o Pendiente.")]
-    public string EstadoTransaccion { get; set; }
+    public string? EstadoTransaccion { get; set; }
 
     [Required(ErrorMessage = "El proveedor del pago es obligatorio.")]
     [RegularExpression(@"^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$", ErrorMessage = "El proveedor solo puede contener letras y espacios.")]
-    public string ProveedorPago { get; set; }
+    public string? ProveedorPago { get; set; }
 }
